eb7e47da298f0281fa5c8e156780778c973b2b16
torikatsupg <torikatsu923@gmail.com>
2023-06-02 16:29:52 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by torii katsuya on 2023/06/02.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
