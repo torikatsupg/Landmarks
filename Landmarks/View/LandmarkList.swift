f1444f8ee37746f0e1946ed0628e0349b37eb0e5
torikatsupg <torikatsu923@gmail.com>
2023-06-03 00:15:50 +0900

    Create LandmarkList

---
+++ Landmarks/View/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by torii katsuya on 2023/06/03.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
