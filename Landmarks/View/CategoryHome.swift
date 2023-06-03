df110f7a1c51f971b1e9ac85221bef31eddca67b
torikatsupg <torikatsu923@gmail.com>
2023-06-03 10:32:46 +0900

    Add Profile views

--- Landmarks/View/CategoryHome.swift
+++ Landmarks/View/CategoryHome.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CategoryHome: View {
     @EnvironmentObject var modelDeta: ModelData
+    @State private var showingProfile = false
     
     var body: some View {
         NavigationView {
@@ -26,7 +27,19 @@ struct CategoryHome: View {
                 .listRowInsets(EdgeInsets())
                 
             }
+            .listStyle(.inset)
             .navigationTitle("Featured")
+            .toolbar {
+                Button {
+                    showingProfile.toggle()
+                } label: {
+                   Label("User Profile", systemImage: "person.crop.circle")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environmentObject(modelDeta)
+            }
         }
     }
 }
